6f8b802e9c9f633d38d6ac0ec8bf8d313984b88a
MalasQ <33825398+MalasQ@users.noreply.github.com>
2017-11-20 20:39:19 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+// простые типы
+
+var  myNumber =200,
+     myString = "Some string",
+     myBool = true,
+     myNull = null,
+     myUndef = undefined;
+
+
+console.log(typeof myNumber);
+console.log(typeof myString);   
+console.log(typeof myBool);   
+console.log(typeof myNull);   
+console.log(typeof myUndef);        
+
+
+
+// объектный типы
+
+
+var obj = {name:"sorax"},
+    array = [1,2,3],
+    regexp = /w+/g,
+    func = function(){};
+
+
+console.log("");
+console.log(typeof obj);
+console.log(typeof array);
+console.log(typeof regexp);
+console.log(typeof func);
